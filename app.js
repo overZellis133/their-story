3b2c61a2a64c8ffb582bfe7cc4146f3c79867ba8
Zack Ellis <zellis125@gmail.com>
2017-06-26 02:27:02 +0000

    Add new route and seed videos

--- app.js
+++ app.js
@@ -1,16 +1,69 @@
-var express = require("express");
-var app = express();
+var methodOverride       = require("method-override"),
+    bodyParser           = require("body-parser"),
+    mongoose             = require("mongoose"),
+    express              = require("express"),
+    app                  = express();
 
+// APP CONFIG
+// mongoose.connect("mongodb://localhost/their_story");
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
+// MONGOOSE/MODEL CONFIG
+var storySchema = new mongoose.Schema({
+   title: String,
+   youtubeID: String,
+   description: String
+});
+
+var Story = mongoose.model("Story", storySchema);
+
+// RESTful ROUTES
+
+// Landing Page
 app.get("/", function(req, res){
-   res.render("landing");
+    
+    res.render("landing");
 });
 
+// INDEX ROUTE
 app.get("/stories", function(req, res){
-   res.render("stories") ;
+    var stories = [
+        {title: "Story Session I", youtubeID: "IjFRdBfEj1I"},
+        {title: "Story Session VI", youtubeID: "SUGgKuO6wpM"},
+        {title: "Story Session VII", youtubeID: "lefmsKYasxA"},
+        {title: "Story Session IIX", youtubeID: "SUGgKuO6wpM"}
+    ];
+    
+   res.render("stories", {stories: stories}) ;
 });
+
+// CREATE ROUTE
+app.post("/stories", function(req, res){
+   var title = req.body.title;
+   var youtubeID = req.body.youtubeID;
+   var newStory = {
+       title: title,
+       youtubeID: youtubeID
+   };
+   
+   Story.create(newStory, function(err, newlyCreated){
+       if(err) {
+           console.log(err);
+       } else {
+           res.redirect("/stories");
+       }
+   });
+});
+
+
+// NEW ROUTE
+app.get("/stories/new", function(req, res){
+   res.render("new");
+});
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Their Story app has started");

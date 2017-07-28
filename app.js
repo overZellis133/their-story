87b661bc51bb0fc30c0ed83e326f7aafbae97cd7
Zack Ellis <zellis125@gmail.com>
2017-07-28 02:08:04 +0000

    Add user authentication and authorization

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var methodOverride       = require("method-override"),
     mongoose             = require("mongoose"),
     express              = require("express"),
     app                  = express(),
+    passport             = require("passport"),
+    LocalStrategy        = require("passport-local"),
     Story                = require("./models/story"),
     Comment              = require("./models/comment"),
     User                 = require("./models/user"),
@@ -17,6 +19,23 @@ app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
 // seedDB();
+
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Any direction you choose...",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+   res.locals.currentUser = req.user;
+   next();
+});
 
 // Landing Page
 app.get("/", function(req, res){
@@ -85,7 +104,7 @@ app.get("/stories/:id", function(req, res){
 // COMMENTS ROUTES
 // =============
 
-app.get("/stories/:id/comments/new", function(req, res){
+app.get("/stories/:id/comments/new", isLoggedIn, function(req, res){
     // find story by id
     Story.findById(req.params.id, function(err, story){
        if (err) {
@@ -96,7 +115,7 @@ app.get("/stories/:id/comments/new", function(req, res){
     });
 });
 
-app.post("/stories/:id/comments", function(req, res){
+app.post("/stories/:id/comments", isLoggedIn, function(req, res){
     // find story by id
     Story.findById(req.params.id, function(err, story){
        if (err) {
@@ -116,6 +135,54 @@ app.post("/stories/:id/comments", function(req, res){
        }
     });
 });
+
+// ===========
+// AUTH ROUTES
+// ===========
+
+// SHOW REGISTER FORM
+app.get("/register", function(req, res){
+   res.render("register");
+});
+
+// handle sign up logic
+app.post("/register", function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if (err) {
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+           res.redirect("/stories");
+        });
+    });
+});
+
+// show login form
+app.get("/login", function(req, res){
+   res.render("login");
+});
+
+app.post("/login", passport.authenticate("local", 
+    {
+        successRedirect: "/stories",
+        failureRedirect: "/login"
+    }), function(req, res){
+});
+
+// logout route
+app.get("/logout", function(req, res){
+   req.logout();
+   res.redirect("/stories");
+});
+
+function isLoggedIn(req, res, next){
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Their Story app has started");
--- models/user.js
+++ models/user.js
@@ -1,8 +1,9 @@
 var mongoose = require("mongoose");
+var passportLocalMongoose = require("passport-local-mongoose");
 
 var userSchema = new mongoose.Schema({
-   email: String,
-   name: String,
+   username: String,
+   password: String,
    stories: [
        {
            type: mongoose.Schema.Types.ObjectId, 
@@ -10,5 +11,7 @@ var userSchema = new mongoose.Schema({
        }
    ]
 });
+
+userSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model("User", userSchema);

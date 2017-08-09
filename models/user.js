26a01e64ef3af0a4d93a5d4bd8707451b099f57f
Zack Ellis <zellis125@gmail.com>
2017-08-09 06:04:18 +0000

    Add email, first name, last name to sign up. If not logged in and redirected to login page after trying to add a comment or story - get redirected back to new story and new comment after logging in

--- models/user.js
+++ models/user.js
@@ -1,8 +1,12 @@
 var mongoose = require("mongoose");
+// var mongooseTypeEmail = require("mongoose-type-email");
 var passportLocalMongoose = require("passport-local-mongoose");
 
 var userSchema = new mongoose.Schema({
    username: String,
+   firstName: String,
+   lastName: String,
+   email: String,
    password: String,
    stories: [
        {
--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,13 @@ router.get("/register", function(req, res){
 
 // handle sign up logic
 router.post("/register", function(req, res){
-    var newUser = new User({username: req.body.username});
+    var newUser = new User(
+        {
+            username: req.body.username,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email
+        });
     User.register(newUser, req.body.password, function(err, user){
         if (err) {
             req.flash("error", err.message)
@@ -38,17 +44,27 @@ router.get("/login", function(req, res){
    res.render("login", {page: "login"});
 });
 
-router.post("/login", passport.authenticate("local", 
-    {
-        successRedirect: "/stories",
-        failureRedirect: "/login"
-    }), function(req, res){
+router.post("/login", function(req, res, next){
+    passport.authenticate('local', function(err, user, info){
+        if(err){
+            return next(err);
+        } if(!user) {
+            req.flash("error", "Username or password is incorrect.");
+            return res.redirect("/login");
+        }
+        req.logIn(user, function(err){
+            if(err){return next(err);}
+            var redirectTo = req.session.redirectTo ? req.session.redirectTo: "/stories";
+            delete req.session.redirectTo;
+            res.redirect(redirectTo);
+        });
+    })(req,res,next);
 });
 
 // logout route
 router.get("/logout", function(req, res){
    req.logout();
-   req.flash("success", "Logged you out!");
+   req.flash("success", "Logged out");
    res.redirect("/stories");
 });
 
